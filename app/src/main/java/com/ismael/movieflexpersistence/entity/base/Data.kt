897dac1c565c9ef7b86836b6c5d904ec74043892
ismalel <34296465+ismalel@users.noreply.github.com>
2021-12-02 12:06:09 -0600

    Fix Entity for API call

--- app/src/main/java/com/ismael/movieflexpersistence/entity/base/Data.kt
+++ app/src/main/java/com/ismael/movieflexpersistence/entity/base/Data.kt
@@ -3,7 +3,7 @@ package com.ismael.movieflexpersistence.entity.base
 class Data (
 
     var page         : Int?          = null,
-    var results      : List<Results> = arrayListOf(),
+    var results      : List<Result> = arrayListOf(),
     var totalPages   : Int?          = null,
     var totalResults : Int?          = null
 
