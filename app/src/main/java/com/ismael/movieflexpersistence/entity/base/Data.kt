abd34c13395c9a9afbfb7476b32ddc0952be037f
ismalel <34296465+ismalel@users.noreply.github.com>
2021-12-01 21:01:37 -0600

    Initial commit

---
+++ app/src/main/java/com/ismael/movieflexpersistence/entity/base/Data.kt
@@ -0,0 +1,10 @@
+package com.ismael.movieflexpersistence.entity.base
+
+class Data (
+
+    var page         : Int?          = null,
+    var results      : List<Results> = arrayListOf(),
+    var totalPages   : Int?          = null,
+    var totalResults : Int?          = null
+
+)
